463c8b0539a8b19b3a2f99be53c0db2fd37abac8
Mohd Al Sumaim <90371883+m-sumaim@users.noreply.github.com>
2023-06-20 12:01:30 +0530

    Update matchsticks-game.cpp

--- Medium/Matchsticks game/matchsticks-game.cpp
+++ Medium/Matchsticks game/matchsticks-game.cpp
@@ -8,6 +8,23 @@ class Solution {
     int matchGame(long long N) {
         // code here
         return N%5 == 0 ? -1 : N%5;
+
+      // Intution:
+        // If N is divisible by 5, it means that no matter how many matchsticks A picks on his first turn,
+        // B can always pick the remaining matchsticks such that A will be left with the last matchstick and lose the game. 
+        // In this case, it is impossible for A to win, so we return -1.
+
+        // If N is not divisible by 5, A can pick a number of matchsticks that leaves a remaining number that is divisible by 5. 
+        // This ensures that A can force B into a position where B will always be left with a number of matchsticks that is divisible by 5,
+        // allowing A to win the game.
+
+        // Therefore, the number of matchsticks A should pick on his first turn is (N % 5). 
+        // This guarantees that A will win the game if N is not divisible by 5. 
+        // Otherwise, it is impossible for A to win, and -1 is returned.
+
+        // Example 1:
+        // If N = 9, A should pick (9 % 5) = 4 matchsticks on his first turn to guarantee a win.
+        // If N = 15, it is impossible for A to win, so -1 is returned.
     }
 };
 
