b1f19e88bc9f87994f7e713c872c702e016d9041
Mohd Al Sumaim <90371883+m-sumaim@users.noreply.github.com>
2023-07-05 18:36:01 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Stock buy and sell II/stock-buy-and-sell-ii.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int stockBuyAndSell(int n, vector<int> &price) {
+        // code here
+        int profit = 0;
+        for(int i=1; i<n; i++)
+        {
+            if(price[i] > price[i-1])
+                profit += price[i]-price[i-1];
+        }
+        return profit;
+        
+        // here we take profit on every pair of prices and keep adding the profits individually
+        // so for [2, 5, 7, 9] we'll have 3+2+2 = 7
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n;
+        scanf("%d",&n);
+        
+        
+        vector<int> prices(n);
+        Array::input(prices,n);
+        
+        Solution obj;
+        int res = obj.stockBuyAndSell(n, prices);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
