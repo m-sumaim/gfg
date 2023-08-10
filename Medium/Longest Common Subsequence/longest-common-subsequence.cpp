7ed1f07a3949852fd1cc30e62ee5def284c8f837
Mohd Al Sumaim <90371883+m-sumaim@users.noreply.github.com>
2023-08-10 12:14:13 +0530

    Updated Solution - GfG to GitHub

--- Medium/Longest Common Subsequence/longest-common-subsequence.cpp
+++ Medium/Longest Common Subsequence/longest-common-subsequence.cpp
@@ -9,42 +9,37 @@ using namespace std;
 class Solution
 {
     public:
-   int dp[1001][1001] ;
-   Solution(){
-       for( int i = 0 ; i < 1001 ; i++ )
-           for( int j = 0 ; j < 1001 ; j++ )
-               dp[i][j] = -1 ;
-   }
-   
-   int lcs(int x , int y , string s1 , string s2 )
+   int lcs(int n , int m , string s1 , string s2 )
    { 
-       if( x == 0 or y == 0 )
-           return dp[x][y] = 0 ;
-           
-       if( dp[x][y] != -1 )
-           return dp[x][y] ;
-           
-       if( s1[x-1] == s2[y-1] )
-           return dp[x][y] = lcs( x - 1 , y - 1 , s1 , s2 ) + 1 ;
-       else
-           return dp[x][y] = max( lcs( x , y - 1 , s1 , s2 ) , lcs( x - 1 , y , s1 , s2 ) ) ;
+        int dp[n+1][m+1];  
+        memset(dp, 0, sizeof(dp));
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=m; j++)
+            {
+                if(s1[i-1] == s2[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
    }
-    
 };
 
 
 //{ Driver Code Starts.
 int main()
 {
-    int t,n,k,x,y;
+    int t,n,m;
     cin>>t;
     while(t--)
     {
-        cin>>x>>y;          // Take size of both the strings as input
+        cin>>n>>m;          // Take size of both the strings as input
         string s1,s2;
         cin>>s1>>s2;        // Take both the string as input
         Solution ob;
-        cout << ob.lcs(x, y, s1, s2) << endl;
+        cout << ob.lcs(n, m, s1, s2) << endl;
     }
     return 0;
 }
