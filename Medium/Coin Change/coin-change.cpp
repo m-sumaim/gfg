7e354255e5a5b86066e338a20bc3e1b059dc6f89
Mohd Al Sumaim <90371883+m-sumaim@users.noreply.github.com>
2023-08-11 12:40:15 +0530

    Updated Solution - GfG to GitHub

--- Medium/Coin Change/coin-change.cpp
+++ Medium/Coin Change/coin-change.cpp
@@ -8,17 +8,14 @@ class Solution {
     long long int count(int coins[], int N, int sum) {
 
         // code here.
-        vector<long long> dp(sum+1, 0);
+        vector<long long int> dp(sum+1, 0);
         dp[0] = 1;
         for(int i=0; i<N; i++)
-        {
-            for(int j=coins[i]; j<= sum; j++)
-                dp[j] += dp[j-coins[i]];
-                
-        }
+            for(int j = coins[i]; j<=sum; j++)
+                dp[j] = dp[j]+dp[j-coins[i]];
+        
         return dp[sum];
     }
-
 };
 
 //{ Driver Code Starts.
